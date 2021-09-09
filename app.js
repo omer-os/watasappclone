3ba14d51ab07f80af423c7e9b4bf8f3535686950
omar chatin <omerchatin19@gmail.com>
2021-09-09 03:43:57 +0300

    adding microphone animation

--- app.js
+++ app.js
@@ -7,9 +7,21 @@ $('.stickersContainer').html(str_emojies).hide()
 $('.sticker').click(()=>{
     $('.stickersContainer').toggle("slow");
 })
-
 function stickerAdd(id){
     let text = $('.msgInput').val()+id.innerHTML
     $('.msgInput').val(text)
 }
+
+$('.soundRecoring').hide()
+
+
+$(".microphone").on('touchstart mousedown', function(e) {
+    $('.soundRecoring').show()
+    e.preventDefault(); //prevents further events from being dispatched
+})
+
+$(".microphone").on('touchend mouseup', function(e) {
+    $('.soundRecoring').hide()
+    e.preventDefault(); //prevents further events from being dispatched
+})
 
